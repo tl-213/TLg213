1e2f8b517641ea4ebda20fb83f86c8f3531075ed
tl-213 <t.lamnguyenz213@gmail.com>
2026-02-05 15:33:54 +0700

    Create upload_images.js

---
+++ js/upload_images.js
@@ -0,0 +1,5 @@
+let image_player1_right = new Image()
+image_player1_right.src = "images/player1_right.png"
+
+let image_player1_left = new Image()
+image_player1_right.src = "images/player1_left.png"
