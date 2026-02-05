0422136c0767a4c77d8a46918ba9ce321b93beee
tl-213 <t.lamnguyenz213@gmail.com>
2026-02-05 20:33:44 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-const canvas = document.getElementById("TL_canvas");
-const ctx = canvas.getContext("2d");
-
 function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawPlayer1();
