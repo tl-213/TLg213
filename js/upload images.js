22335448c5a87c3d33a2fcb3b2460bff73993704
tl-213 <t.lamnguyenz213@gmail.com>
2026-02-05 15:33:34 +0700

    Delete js/upload images.js

--- js/upload images.js
+++
@@ -1,5 +0,0 @@
-let image_player1_right = new Image()
-image_player1_right.src = "images/player1_right.png"
-
-let image_player1_left = new Image()
-image_player1_right.src = "images/player1_left.png"
