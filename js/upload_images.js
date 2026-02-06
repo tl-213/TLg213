d780c809150c77f7f870f859618607ecf9db548a
tl-213 <t.lamnguyenz213@gmail.com>
2026-02-06 14:38:24 +0700

    Update upload_images.js

--- js/upload_images.js
+++ js/upload_images.js
@@ -2,4 +2,12 @@ let image_char1_right = new Image()
 image_char1_right.src = "images/character1_vietnamball_right.png"
 
 let image_char1_left = new Image()
-image_char1_right.src = "images/images/character1_vietnamball_right.png"
+image_char1_right.src = "images/character1_vietnamball_right.png"
+
+
+
+let image_normal_zombie_001_right = new Image()
+image_normal_zombie_001_right.src = "images/normal_zombie_001_right.png"
+
+let image_normal_zombie_001_left = new Image()
+image_normal_zombie_001_left.src = "images/normal_zombie_001_left.png"
