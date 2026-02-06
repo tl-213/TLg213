d11a6059823a43b9c2d4acf5b0573a13ac1ce300
tl-213 <t.lamnguyenz213@gmail.com>
2026-02-06 14:02:46 +0700

    Create spawn_npc.js

---
+++ js/spawn_npc.js
@@ -0,0 +1,33 @@
+let npc = {
+  monster: {
+    zombies: {
+      normal: {
+        normal_zombie_001: {
+          image,
+          color_effect: "white",
+          level: 1,
+
+          damage: 5,
+          canAttack: true,
+
+          hp: {
+            hp_now: 20,
+            hp_max: 20
+          },
+
+          position: {
+            x: 0,
+            y: 0
+          },
+
+          move: {
+            direction: "right",
+            speed: 3
+          }
+        },
+
+        zombie_speed_001
+      }
+    }
+  }
+}
