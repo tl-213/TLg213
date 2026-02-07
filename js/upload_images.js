5c706c1a00f8ad0c5bc54ae57a55340848c68090
tl-213 <t.lamnguyenz213@gmail.com>
2026-02-07 19:36:20 +0700

    Update upload_images.js

--- js/upload_images.js
+++ js/upload_images.js
@@ -1,23 +1,23 @@
-let image_char1_right = new Image()
-image_char1_right.src = "images/player_images/character1_vietnamball_right.png"
+let image_char1_right = new Image();
+image_char1_right.src = "images/player_images/character1_vietnamball_right.png";
 
-let image_char1_left = new Image()
-image_char1_left.src = "images/player_images/character1_vietnamball_left.png"
+let image_char1_left = new Image();
+image_char1_left.src = "images/player_images/character1_vietnamball_left.png";
 
 
 
 let zombies = {
 	image_normal_zombie_001_right: new Image(),
 	image_normal_zombie_001_left: new Image()
-}
+};
 
-zombies.image_normal_zombie_001_right.src = "images/zombies/normal_zombie_001/normal_zombie_001_right.png"
-zombies.image_normal_zombie_001_left.src = "images/zombies/normal_zombie_001/normal_zombie_001_left.png"
+zombies.image_normal_zombie_001_right.src = "images/zombies/normal_zombie_001/normal_zombie_001_right.png";
+zombies.image_normal_zombie_001_left.src = "images/zombies/normal_zombie_001/normal_zombie_001_left.png";
 
 
 
 let image_background = {
 	image_background_map_001: new Image()
-}
+};
 
-image_background.image_background_map_001.src = "images/background/Map 001.png"
+image_background.image_background_map_001.src = "images/background/Map 001.png";
