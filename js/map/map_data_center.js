a77acb52fb04221fab3c0279d87faeaec0a69a8c
tl-213 <t.lamnguyenz213@gmail.com>
2026-02-07 21:34:11 +0700

    Update map_data_center.js

--- js/map/map_data_center.js
+++ js/map/map_data_center.js
@@ -2,6 +2,8 @@ map_data_center = {
   map_format_code: 1
 };
 
-if (map_format_code === 1) {
-  create_NormalMap_001()
+function create_normal_map() {
+  if (map_format_code === 1) {
+    create_NormalMap_001()
+  }
 }
