58948f33f7eb8b3288b59d5c1bd031303372871b
tl-213 <t.lamnguyenz213@gmail.com>
2026-02-07 19:24:48 +0700

    Update upload_images.js

--- js/upload_images.js
+++ js/upload_images.js
@@ -1,16 +1,18 @@
 let image_char1_right = new Image()
-image_char1_right.src = "images/character1_vietnamball_right.png"
+image_char1_right.src = "images/player_images/character1_vietnamball_right.png"
 
 let image_char1_left = new Image()
-image_char1_left.src = "images/character1_vietnamball_left.png"
+image_char1_left.src = "images/player_images/character1_vietnamball_left.png"
 
 
 
-let image_normal_zombie_001_right = new Image()
-image_normal_zombie_001_right.src = "images/normal_zombie_001_right.png"
+let zombies = {
+	image_normal_zombie_001_right: new Image(),
+	image_normal_zombie_001_left: new Image()
+}
 
-let image_normal_zombie_001_left = new Image()
-image_normal_zombie_001_left.src = "images/normal_zombie_001_left.png"
+zombies.image_normal_zombie_001_right.src = "images/zombies/normal_zombie_001/normal_zombie_001_right.png"
+zombies.image_normal_zombie_001_left.src = "images/zombies/normal_zombie_001/normal_zombie_001_left.png"
 
 
 
