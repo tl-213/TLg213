68564b6f6046d10ec091f68ce1fa4d5a6de4777e
tl-213 <t.lamnguyenz213@gmail.com>
2026-02-07 21:28:21 +0700

    Update map_01_to_10.js

--- js/map/maps/normal/map_01_to_10.js
+++ js/map/maps/normal/map_01_to_10.js
@@ -1,2 +1,7 @@
 function create_NormalMap_001() {
+  ctx.beginPath()
+  
+  ctx.drawImage(image_background.image_background_map_001, 
+                player1_pos.x, 
+                player1_pos.y)
 }
