6c6290e0ff03b416d55607808be4604059ea47e1
tl-213 <t.lamnguyenz213@gmail.com>
2026-02-08 21:02:31 +0700

    Update avatar_mana_health.js

--- js/player/avatar_mana_health.js
+++ js/player/avatar_mana_health.js
@@ -3,8 +3,14 @@ function draw_AvatarHealthbarManabar() {
 	
 	ctx.drawImage(avatar_and_bar.frameBar_bg_image, 0, 0)
 
-	ctx.fillStyle = "green"
+	ctx.fillStyle = "rgb(60, 255, 20)"
 	ctx.fillRect(130, 55, 211 * (player.hp.hp_now / player.hp.hp_max), 27)
+	ctx.fill()
+
+	ctx.beginPath()
+	ctx.fillStyle = "rgba(0, 55, 255, 1)"
+	ctx.fillRect(110, 90, 182 * (player.mana.mana_now / player.mana.mana_max), 16)
+	ctx.fill()
 
 	ctx.drawImage(avatar_and_bar.avatar_frameBar_image, 0, 0)
 }
