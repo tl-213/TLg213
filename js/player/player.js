2781f2d34c42d25718423258788c2abcabe664a2
tl-213 <t.lamnguyenz213@gmail.com>
2026-02-08 15:00:54 +0700

    Create player.js

---
+++ js/player/player.js
@@ -0,0 +1,68 @@
+
+let EventKeydown_of_player = {
+  move: {
+    normal: "o"
+  },
+
+  player_direction: "right"  
+};
+
+let player1 = {
+  speed: 5,
+  attack_range: 45,
+
+  hp: {
+    hp_now: 100,
+    hp_max: 100
+  },
+
+  rect_hp: {
+    rect_w: 200,
+    rect_h: 30
+  }
+};
+
+function drawPlayer1() {
+  if (EventKeydown_of_player.move.normal === "w") {
+    player1_pos.y -= player1.speed;
+  } 
+  else if (EventKeydown_of_player.move.normal === "s") {
+    player1_pos.y += player1.speed;
+  } 
+  else if (EventKeydown_of_player.move.normal === "a") {
+    player1_pos.x -= player1.speed;
+    EventKeydown_of_player.player_direction = "left";
+  } 
+  else if (EventKeydown_of_player.move.normal === "d") {
+    player1_pos.x += player1.speed;
+    EventKeydown_of_player.player_direction = "right";
+  }
+
+  ctx.beginPath();
+  
+  if (EventKeydown_of_player.player_direction === "left") {
+    ctx.drawImage(image_char1_left, canvas.width / 2, canvas.height / 2);
+  }
+  else if (EventKeydown_of_player.player_direction === "right") {
+    ctx.drawImage(image_char1_right, canvas.width / 2, canvas.height / 2);
+  }
+  
+  ctx.fill();
+}
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "a" || e.key === "d" || e.key === "w" || e.key === "s") {
+    EventKeydown_of_player.move.normal = e.key;
+  }
+});
+
+window.addEventListener("keyup", (e) => {
+  if (
+    e.key.toLowerCase() === "w" ||
+    e.key.toLowerCase() === "s" ||
+    e.key.toLowerCase() === "a" ||
+    e.key.toLowerCase() === "d"
+  ) {
+    EventKeydown_of_player.move.normal = "o";
+  }
+});
