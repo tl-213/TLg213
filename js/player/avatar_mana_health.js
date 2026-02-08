9893fa35b74527b2baff713ccd6e37f82e6ff536
tl-213 <t.lamnguyenz213@gmail.com>
2026-02-08 19:13:37 +0700

    Update avatar_mana_health.js

--- js/player/avatar_mana_health.js
+++ js/player/avatar_mana_health.js
@@ -4,7 +4,7 @@ function draw_AvatarHealthbarManabar() {
 	ctx.drawImage(avatar_and_bar.frameBar_bg_image, 0, 0)
 
 	ctx.fillStyle = "green"
-	ctx.fillRect(211 * (player.hp_now / player.hp_max), 27, 15, 10)
+	ctx.fillRect(130, 55, 211 * (player.hp.hp_now / player.hp.hp_max), 27)
 
 	ctx.drawImage(avatar_and_bar.avatar_frameBar_image, 0, 0)
 }
