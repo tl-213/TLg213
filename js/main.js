f002c6e6213948c2d2f01e87424b1cc8c6afad39
tl-213 <t.lamnguyenz213@gmail.com>
2026-02-08 15:02:14 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   create_normal_map();
+  draw_AvatarHealthbarManabar();
   drawPlayer1();
   requestAnimationFrame(gameLoop);
 }
