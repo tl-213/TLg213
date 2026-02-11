01c2b6eed6c709f0979fb78730a05f410d320dec
tl-213 <t.lamnguyenz213@gmail.com>
2026-02-11 20:57:11 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  resize_canvas()
+  resize_canvas();
   create_normal_map();
   draw_AvatarHealthbarManabar();
   drawPlayer();
