ca462adb0d3784b4ccc36c019f5bc9cd9c88e3de
tl-213 <t.lamnguyenz213@gmail.com>
2026-02-13 14:22:52 +0700

    Update player_data_center.js

--- js/player/player_data_center.js
+++ js/player/player_data_center.js
@@ -1,1 +1,26 @@
+let player = {
+  speed: 5,
+  attack_range: 45,
 
+  hp: {
+    hp_now: 100,
+    hp_max: 100
+  },
+
+  mana: {
+    mana_now: 100,
+    mana_max: 100
+  },
+
+  rect_hp: {
+    rect_w: 200,
+    rect_h: 30
+  }
+};
+
+let player_pos = {
+  x: 0,
+  y: 0,
+  w: 60,
+  h: 60
+};
